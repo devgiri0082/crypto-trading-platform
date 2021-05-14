18ea48a9f2b2c72b6d104f75eff50b5c4a9e7cca
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-14 18:26:46 +0800

    made form component

---
+++ src/Components/Form.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+let MainDiv = styled.div`
+
+`
+let Header = style.div`
+
+`
+
+export default function Form(props) {
+    return (
+        <MainDiv>
+            <Header>
+                <
+            </Header>
+        </MainDiv>
+    )
+}
