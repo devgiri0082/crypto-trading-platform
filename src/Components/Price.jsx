8fdf2b4ff647a9d8aca94aba1f306014d1bc1c68
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-14 15:08:21 +0800

    added fetch

--- src/Components/Price.jsx
+++ src/Components/Price.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import CoinContext from "../Context/CoinContext";
 
 export default function Price() {
-    return (
+  const API =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20dogecoin";
+  async function getPrices() {
+    let response = await fetch(API);
+    let data = await response.json();
+    let temp = {}
+      data.forEach(el => temp[el.id] = { name: el.name, current_price: el.current_price, market_cap: (el.market_cap_change_24h).toFixed(5) })
+      
+    }
+    
+    let [prices, setPrices] = useState({})
+  return (
+      <CoinContext.Provider value={{ Prices: prices}}>
         <div>
-            
-        </div>
-    )
+              
+      </div>
+    </CoinContext.Provider>
+  );
 }
---
+++ src/Context/CoinContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+
+let CoinContext = createContext()
+
+export default CoinContext
