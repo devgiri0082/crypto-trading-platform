9d57d7da09f27f82fe21fe55afe60c213a54e2b2
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-14 18:59:27 +0800

    commit test

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,30 +1,33 @@
-import { useState } from "react";
-import styled from "styled-components";
+import { useState } from "react"
+import styled from "styled-components"
 
-let MainDiv = styled.div``;
-let Header = styled.div``;
+let MainDiv = styled.div`
+`
+let Header = styled.div`
+`
 
 let FormBody = style.div`
-
-`;
+`
 
 export default function Form(props) {
-  let [type, setType] = useState("Buy");
-  let [coinName, currentPrice] = ["Dogecoin", 0.511823];
 
-  return (
-    <MainDiv>
-      <Header>
-        <div className="title">{type + " " + coinName}</div>
-        <button>x</button>
-      </Header>
-      <FormBody>
-        <p>Current Price: ${currentPrice}</p>
-        <label>
-          <input type="text" placeholder="0" />
-          Max:
-        </label>
-      </FormBody>
-    </MainDiv>
-  );
+    let [type, setType] = useState('Buy')
+    let [coinName, currentPrice] = ['Dogecoin', 0.511823]
+
+    return (
+        <MainDiv>
+            <Header>
+                <div className="title">{type + ' ' + coinName}</div>
+                <button>x</button>
+
+            </Header>
+            <FormBody>
+                <p>Current Price: ${currentPrice}</p>
+                <label>
+                    <input type="text" placeholder='0' />
+                    Max:
+                </label>
+            </FormBody>
+        </MainDiv>
+    )
 }
