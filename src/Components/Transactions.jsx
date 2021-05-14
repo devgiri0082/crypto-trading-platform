0de6dd496ca5eb00db22d3af8eee7f7e5212fb22
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-14 17:46:17 +0800

    construct transaction component

--- src/Components/Transactions.jsx
+++ src/Components/Transactions.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+import EachTransactions from './EachTransactions'
+
+let MainDiv = styled.div`
+
+`
+
+let TransactionList = styled.div`
+
+`
 
 export default function Transactions() {
+    
     return (
-        <div>
-            
-        </div>
+        <MainDiv>
+            <h1>Transactions</h1>
+            <TransactionList>
+                <EachTransactions />
+            </TransactionList>
+        </MainDiv>
     )
 }
