6ae2f6cf8d51701e67f5b30eab7fc9129f6cb5d8
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-14 17:13:51 +0800

    construct prices component

--- src/Components/EachPrice.jsx
+++ src/Components/EachPrice.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+import CoinContext from '../Context/CoinContext'
 
 let Card = styled.div`
     display: flex;
+    box-shadow: 1px 1px 5px rgb(182, 181, 181);
+    padding: 0.5em 1em;
+    border-radius: 10px;
+    background-color: white;
+    align-items: center;
+    gap: 1em;
 `
 
 let Logo = styled.img`
-    width: 50px;
-    height: 50px;
+    width: 70px;
+    height: 70px;
 `
 
-export default function EachPrice() {
+let Details = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+        font-size: 1.5rem;
+    }
+
+    .name {
+        font-weight: 400;
+    }
+
+    .cap {
+        font-size: .8rem;
+    }
+`
+
+export default function EachPrice({ coinName }) {
+    let { Coin } = useContext(CoinContext)
+    let {image, current_price, name, market_cap} = Coin[coinName]
     return (
         <Card>
-
+            <Logo src={image} />
+            <Details>
+            <h1>${current_price}</h1>
+            <p className='name'><b>{name}</b></p>
+            <p className='cap'>Last 24h: <span style={{color: market_cap > 0 ? 'green' : 'red'}} >{market_cap}%</span></p>
+            </Details>
         </Card>
     )
 }
--- src/Components/Price.jsx
+++ src/Components/Price.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import CoinContext from "../Context/CoinContext";
 import EachPrice from "./EachPrice";
+import styled from 'styled-components'
+
+let PriceDiv = styled.div`
+    display: grid;
+    width: 60%;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    gap: 0.5em;
+`
 
 export default function Price() {
+    let { Coin } = useContext(CoinContext)
   return (
-    <div>
-          <EachPrice />
-          <EachPrice />
-          <EachPrice />
-    </div>
+    <PriceDiv>
+          {Object.keys(Coin).map(el => <EachPrice coinName={el} />)}
+    </PriceDiv>
   );
 }
