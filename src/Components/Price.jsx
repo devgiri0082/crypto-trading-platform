9fa7dad0b9ff44142e7e4aeac79facdbb19d135c
dev giri <devgiri0082@gmail.com>
2021-05-14 13:14:06 +0545

    working on the heading

--- src/Components/Price.jsx
+++ src/Components/Price.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CoinContext from "../Context/CoinContext";
 
 export default function Price() {
@@ -8,15 +8,15 @@ export default function Price() {
     let response = await fetch(API);
     let data = await response.json();
     let temp = {}
-      data.forEach(el => temp[el.id] = { name: el.name, current_price: el.current_price, market_cap: (el.market_cap_change_24h).toFixed(5) })
-      
-    }
-    
-    let [prices, setPrices] = useState({})
+    data.forEach(el => temp[el.id] = { name: el.name, current_price: el.current_price, market_cap: (el.market_cap_change_24h).toFixed(5) })
+
+  }
+
+  let [prices, setPrices] = useState({});
   return (
-      <CoinContext.Provider value={{ Prices: prices}}>
-        <div>
-              
+    <CoinContext.Provider value={{ Prices: prices }}>
+      <div>
+
       </div>
     </CoinContext.Provider>
   );
