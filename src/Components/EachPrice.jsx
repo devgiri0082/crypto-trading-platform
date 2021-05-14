e0ddcaf8fc504e50066594fdbcb846929c67a3e2
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-14 16:36:22 +0800

    EAchPrice

--- src/Components/EachPrice.jsx
+++ src/Components/EachPrice.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+let Card = styled.div`
+    display: flex;
+`
+
+let Logo = styled.img`
+    width: 50px;
+    height: 50px;
+`
 
 export default function EachPrice() {
     return (
-        <div>
-            
-        </div>
+        <Card>
+
+        </Card>
     )
 }
--- src/Components/Price.jsx
+++ src/Components/Price.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import CoinContext from "../Context/CoinContext";
+import EachPrice from "./EachPrice";
 
 export default function Price() {
   return (
     <div>
-
+          <EachPrice />
+          <EachPrice />
+          <EachPrice />
     </div>
   );
 }
