10caa0aad505e8d25f4860de6f7121557749cdad
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-14 18:10:42 +0800

    Finished transactions design

--- src/Components/EachTransactions.jsx
+++ src/Components/EachTransactions.jsx
@@ -2,18 +2,40 @@ import React from 'react'
 import styled from 'styled-components'
 
 let MainDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    box-shadow: 1px 1px 5px rgb(182, 181, 181);
+    padding: 0.5em 1em;
+    border-radius: 8px;
+    background-color: white;
+    justify-content: center;
+    font-weight: 400;
+    gap: 0.2em;
+    width: 100%;
+    color: #505050;
 
+    &.Bought {
+        border-left: 8px solid #24b324;
+    }
+
+    &.Sold {
+        border-left: 8px solid #b32424;
+    }
+
+    .date {
+        font-size: .8rem;
+    }
 `
 
 export default function EachTransactions() {
 
-    let [ type, coinName, quantity, currentPrice, date] = ['Bought', 'Dogecoin', 50, 0.511823, (new Date()).getTime()]
+    let [ type, coinName, quantity, currentPrice, date] = ['Bought', 'Dogecoin', 50, 0.511823, (new Date())]
 
     return (
-        <MainDiv>
+        <MainDiv className={type}>
             <p>{coinName + ' ~ ' + quantity + '@' + currentPrice}</p>
-            <p>Paid: {parseFloat((quantity * currentPrice).toFixed(2))}</p>
-            <p className="date">{type} on {(new Intl.DateTimeFormat('en', date)).format()}</p>
+            <p>Paid: ${parseFloat((quantity * currentPrice).toFixed(2))}</p>
+            <p className="date">{type} on {date.toLocaleString()}</p>
         </MainDiv>
     )
 }
--- src/Components/Transactions.jsx
+++ src/Components/Transactions.jsx
@@ -3,10 +3,21 @@ import styled from 'styled-components'
 import EachTransactions from './EachTransactions'
 
 let MainDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    padding: 1em;
 
+    h1 {
+        font-weight: 500;
+    }
 `
 
 let TransactionList = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: .5em;
 
 `
 
@@ -16,6 +27,7 @@ export default function Transactions() {
         <MainDiv>
             <h1>Transactions</h1>
             <TransactionList>
+                <EachTransactions />
                 <EachTransactions />
             </TransactionList>
         </MainDiv>
