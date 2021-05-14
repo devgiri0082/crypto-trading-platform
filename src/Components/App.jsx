a4f1d94e18cf3a097149b9c84f26700ba808b4f5
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-14 15:23:38 +0800

    moved context to main

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
-import styled from 'styled-components'
-import Header from './Header'
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+import CoinContext from "../Context/CoinContext";
+import { useState, useEffect } from "react";
 
 let Container = styled.div`
-    height: 100vh;
-    width: 100vw;
-    background-image: url("images/bg.svg");
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 30px;
-`
+  height: 100vh;
+  width: 100vw;
+  background-image: url("images/bg.svg");
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px;
+`;
 export default function App() {
-    return (
-        <Container>
-            <Header />
-        </Container>
-    )
+  const API =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20dogecoin";
+
+  let [prices, setPrices] = useState({});
+
+  async function getPrices() {
+    let response = await fetch(API);
+    let data = await response.json();
+    let temp = {};
+    data.forEach(
+      (el) =>
+        (temp[el.id] = {
+          name: el.name,
+          current_price: el.current_price,
+          market_cap: el.market_cap_change_24h.toFixed(5),
+        })
+    );
+    setPrices(temp);
+    }
+    
+    useEffect(() => {
+        getPrices()
+    }, [])
+
+  return (
+    <CoinContext.Provider value={{ Prices: prices, Wallet: 100 }}>
+      <Container>
+        <Header />
+      </Container>
+    </CoinContext.Provider>
+  );
 }
