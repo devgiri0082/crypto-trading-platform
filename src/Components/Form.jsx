5d0a8956942dd68420840b99e46e8f03f362aedb
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-05-14 19:34:41 +0800

    finished form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,11 +2,63 @@ import { useState } from "react"
 import styled from "styled-components"
 
 let MainDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 350px;
+    box-shadow: 1px 1px 5px rgb(182, 181, 181);
+    margin-bottom: 5em;
+    border-radius: 10px;
 `
 let Header = styled.div`
-`
+    height: 15%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    color: white;
+    background-color: #353434;
+    border-radius: 10px 10px 0 0;
 
-let FormBody = style.div`
+    button {
+        background-color: transparent;
+        border: none;
+        color: white;
+        font-weight: 600;
+    }
+`
+let FormBody = styled.div`
+    height: 100%;
+    color: #505050;
+    padding: 15px 15px;
+    display: flex;
+    flex-direction: column;
+    gap: .5em;
+    align-items: flex-start;
+    justify-content: center;
+    background-color: white;
+    border-radius: 0 0 10px 10px;
+
+    label {
+        display: flex;
+        gap: .5em;
+        align-items: center;
+    }
+
+    .amt {
+        height: 2.5em;
+        padding: 0em .5em;
+        border: 2px solid #505050;
+        border-radius: 5px;   
+        font-family: "Poppins";
+    }
+
+    button {
+        padding: .5em 1em;
+        border: none;
+        background-color: #353434;
+        color: white;
+        border-radius: 5px;
+    }
 `
 
 export default function Form(props) {
@@ -18,15 +70,21 @@ export default function Form(props) {
         <MainDiv>
             <Header>
                 <div className="title">{type + ' ' + coinName}</div>
-                <button>x</button>
-
+                <button>X</button>
             </Header>
             <FormBody>
                 <p>Current Price: ${currentPrice}</p>
                 <label>
-                    <input type="text" placeholder='0' />
-                    Max:
+                    <input className='amt' type="text" placeholder='0' />
+                    Max: 195.123456
                 </label>
+                <label>
+                <input type="radio" name='type' value='Buy' /> Buy
+                </label>
+                <label>
+                <input type="radio" name='type' value='Sell' /> Sell
+                </label>
+                <button>{type}</button>
             </FormBody>
         </MainDiv>
     )
